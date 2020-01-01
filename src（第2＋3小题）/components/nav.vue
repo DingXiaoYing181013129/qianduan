d682cd63cbbb2aa0e249101aa1066fdec4c31650
丁晓莹 <fj111222333@163.com>
2020-01-02 03:30:29 +0800

    上传第2和3小题的src

---
+++ src（第2＋3小题）/components/nav.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="mui-content">
+		        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="HelloWorld">
+		                    
+<svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-xueren"></use>
+</svg>
+		                    <div class="mui-media-body">Blessing</div></router-link></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="card">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-shengdanhuahuan"></use>
+</svg>
+		                    <div class="mui-media-body">Flowers</div></router-link></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		            	<svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-shengdanlaoren"></use>
+</svg>
+		                    <div class="mui-media-body">Cap</div></a></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-shengdanwa"></use>
+</svg>
+		                    <div class="mui-media-body">Stocking</div></a></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-liwu"></use>
+</svg>
+		                    <div class="mui-media-body">Gift</div></a></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-xinfeng"></use>
+</svg>
+		                    <div class="mui-media-body">Envelope</div></a></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-tangguo"></use>
+</svg>
+		                    <div class="mui-media-body">Sugar</div></a></li>
+		            
+		            
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-shengdanshu"></use>
+</svg>
+		                    <div class="mui-media-body">Tree</div></a></li>
+		           
+		           
+		           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-shengdanmao"></use>
+</svg>
+		                    <div class="mui-media-body">Snowman</div></a></li>
+		        </ul> 
+		</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.icon {
+  width: 2.5em;
+  height: 2.5em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
